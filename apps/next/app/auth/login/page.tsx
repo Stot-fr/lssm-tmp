5968b462f67cc03690fcf86f0f4bfa3e4bb8f139
Theo Boutron <contact@tboutron.pro>
2024-10-18 13:44:03 +0200

    add example structure

---
+++ apps/next/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+// import { ViewContactDetailPage } from "bjeojbe";
+
+const Page = (urlParams: any) => {
+  return (
+    <ViewContactDetailPage
+        userId={urlParams.userId}
+    />
+  );
+};
+
+export default Page;
