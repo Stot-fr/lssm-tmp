20ee3cbb17439acef795deb15c2c4d28f33f5d88
Theo Boutron <contact@tboutron.pro>
2024-10-28 09:08:57 +0100

    fix: coderabbit issue

--- apps/api/src/messaging/conversation.service.ts
+++ apps/api/src/messaging/conversation.service.ts
@@ -4,7 +4,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ConversationService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   async getMyConversations() {
     return this.prisma.conversation.findMany();
